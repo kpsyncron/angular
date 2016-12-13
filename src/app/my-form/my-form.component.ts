32a9cffae603cbabec517b6e7c7f1c632a23edb8
Krzysztof Panek <krzysztof.panek@syncron.com>
2016-12-13 15:15:54 +0100

    Template-driven Form

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-form.component.css']
 })
 export class MyFormComponent implements OnInit {
+  private model : Person = 
+    new Person("Evil", 42);
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+class Person {
+  constructor(
+    public name : string,
+    public age : number
+  ) {} 
 }
