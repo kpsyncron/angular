16794fb8702114149accafe702bc3a306e838bf1
Krzysztof Panek <krzysztof.panek@syncron.com>
2016-12-13 11:17:31 +0100

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  titleColor() : string {
+    return (this.title.length % 2 === 0) ?
+      'gray' : 'blue';
+  }
 }
